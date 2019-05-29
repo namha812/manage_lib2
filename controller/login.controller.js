7c8a4a828228b5f180e0da90a4d240a9e6111e88
tan pham <tan.pham@setacinq.vn>
2019-05-29 13:13:31 +0700

    update

--- controller/login.controller.js
+++ controller/login.controller.js
@@ -6,14 +6,14 @@ module.exports = {
     login: async function(req, res) {
         let user = await models.user.findOne({
             where: {
-                email: 'namha812@gmail.com',
-                password: '1234'
+                email: req.body.userName,
+                password: req.body.password
             },
             attributes: ['id','fullName', 'email']
         });
         if(user) {
             var token = jwt.sign(user.dataValues, secretKey, {expiresIn: '10h'});
-            res.send({token: token, code: 'SUCCESS', message: "login success", user: user});
+            res.send({token: 'JWT ' + token, code: 'SUCCESS', message: "login success", user: user});
         }else {
             res.send(message.BadRequest(req, res));
         }
--- routes/api/login.js
+++ routes/api/login.js
@@ -2,5 +2,5 @@ var express = require('express');
 var router = express.Router();
 const loginController = require('../../controller/login.controller');
 /* GET users listing. */
-router.post('/', loginController.login)
+router.post('/', loginController.login);
 module.exports = router;
